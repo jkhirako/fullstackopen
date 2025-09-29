746a08bb464fdad9422229f9bfbb0c0a4b2cf707
Joshua Kenta Remulta <jkhirako@gmail.com>
2025-09-29 20:55:41 +0800

    full stack phonebook

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -8,6 +8,7 @@ app.use(
 morgan.token("body", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
 });
+app.use(express.static("dist"));
 
 let phonebook = [
   {
@@ -96,7 +97,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
